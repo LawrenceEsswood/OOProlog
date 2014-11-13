4dc3a8ee4c63b203776e57521a4ae8b6b45bce4b
LawrenceEsswood <le277@cam.ac.uk>
2014-11-13 16:41:30 +0000

    Seeing some output from the generator

--- src/generate.pl
+++ src/generate.pl
@@ -1,14 +1,21 @@
 :- ['./parse.pl'].
 
-main :- open('./tests/test.oopl', read ,S) , parse_defs((S, user_output, _), Defs, end_of_file), !,
-		open('./tests/test.out.pl', write ,S2) generate_defs_top((user_input, S2, _), Defs, top).
+main :- 
+	open('./tests/test2.oopl', read , S), parse_defs((S, user_output, _), Defs, end_of_file), !,
+	open('./tests/test2.out.pl', write ,S2), generate_defs_top((user_input, S2, _), Defs, top).
+
+
+% Helper predicates (these should be moved into their own file)
+list_to_comma_functor([X],X).
+list_to_comma_functor([X,Y|Zs], (X,C)) :- list_to_comma_functor([Y|Zs], C).
 
 % TODO
 make_class(S, Def, Scope) :- 
-	get_class_name(Def, Scope, Name),
+	get_class_name(Def, Name),
 	make_class_type_functor(Name, Scope, F),
 	make_class_type_body(Name, Scope, Bdy),
-	P =.. [F, Bdy].
+	P =.. [F, Bdy],
+	out_stream(S, Out), write(Out, P), write(Out, '.\n').
 
 make_predicate_name(PredicateName, Scope, Result) :- scope_name(Scope, Prefix), atomic_list_concat([Prefix, PredicateName], Result).
 
@@ -20,7 +27,7 @@ make_class_type_body(Name, Scope, blah_blah).
 % Generates prolog from a single definition
 make_from_fact(Nm, ArgLst, [], Res) :- Res =.. [Nm|ArgLst].
 % FIXME this might not print properly
-make_from_fact(Nm, ArgLst, Commands, (P1 :- P2)) :- Commands \= [], P1 =.. [Nm|ArgLst], P2 =.. [','|Commands].
+make_from_fact(Nm, ArgLst, Commands, (P1 :- P2)) :- Commands \= [], P1 =.. [Nm|ArgLst], list_to_comma_functor(Commands, P2).
 
 % Gets a name from a particular definition
 get_class_name(class_def(class_head(X,_),_), X).
@@ -55,24 +62,25 @@ generate_defs(S, [Def|Defs], Scope) :- generate_def(S, Def, Scope), generate_def
 generate_defs(_, [], _).
 
 % looks up a name to get a prefix
-lookup(scp(Prefix, Names, Next), Arg, Prefix) :- contains(Names, Arg), !.
-lookup(scp(Prefix, Names, Next), Arg, Res) :- lookup(Next, Arg, Res).
+lookup(scp(Prefix, Names, _), Arg, Prefix) :- member(Arg, Names), !.
+lookup(scp(_, _, Next), Arg, Res) :- lookup(Next, Arg, Res).
 
 %Atoms might refer to a class
-resolve_arg(Scope, atm(X), Tmp, [C]) :- 
+resolve_arg(Scope, atom(X), Tmp, [C]) :-
 	lookup(Scope, class(X), Prefix),!,
 	make_class_type_functor(X, Scope, F),
-	C ..= [F,'This',Tmp].
+	C =.. [F,'This',Tmp].
 
-resolve_arg(_, atm(X), X, []).
+resolve_arg(_, atom(X), X, []) :- !.
 
 %Variables might be members
-resolve_arg(Scope, var(Arg), Tmp, [::(This, Arg, Tmp)]) :-
-	lookup(Scope, field(Arg), X), !.
-resolve_arg(_, var(Arg), Arg, []).
+resolve_arg(Scope, var(Arg), Tmp, [::('This', ArgC, Tmp)]) :-
+	lookup(Scope, field(Arg), _), !,
+	atomic_list_concat(['\'', Arg, '\''], ArgC).
+resolve_arg(_, var(Arg), Arg, []) :- !.
 
 % Tmps will never mean anything in context
-resolve_arg(Scope, tmp(Arg), Arg, []).
+resolve_arg(_, tmp(Arg), Arg, []) :- !.
 
 % This is the case where we have a compound, might be need to add a prefix
 resolve_arg(Scope, P, Pd, Commands) :- 
@@ -80,26 +88,20 @@ resolve_arg(Scope, P, Pd, Commands) :-
 	lookup(Scope, pred(Nm), Prefix), !,
 	make_predicate_name(Nm, Prefix, NewName),
 	resolve_args(Scope, Args, Argsd, Commands),
-	Pd ..= [NewName|Argsd].
+	Pd =.. [NewName|Argsd].
 resolve_arg(Scope, P, Pd, Commands) :-
 	P =.. [Nm|Args],
 	resolve_args(Scope, Args, Argsd, Commands),
-	Pd ..= [Nm|Argsd].
+	Pd =.. [Nm|Argsd].
 
 resolve_args(Scope, [Arg|Args],[NewArg|NewArgs],AllCommands) :-
 	resolve_arg(Scope, Arg, NewArg,Commands), 
 	resolve_args(Scope, Args, NewArgs, Commands2),
 	append(Commands, Commands2, AllCommands).
 
-resolve_args([],[],[]).
+resolve_args(_,[],[],[]).
 
-generate_def(S, fact(P), Scope) :- 
-	P =.. [Nm|ArgLstIn], resolve_args(Scope, ArgLstIn, ArgLst, Commands),
-	make_predicate_name(Nm, Scope, NewName),
-	make_from_fact(NewName, ArgLst, Commands, Res),
-	out_stream(S, Out), write(Out, Res), write(Out, '\n').
-
-resolve_args_comma_functor(S, B, Scope, Res) :- 
+resolve_args_comma_functor(S, B, Scope, Res) :-
 	functor(B, ',', _),
 	arg(1, B, Arg), arg(2, B, Bdy),
 	resolve_arg(Scope, Arg, NewArg, Commands1),
@@ -108,23 +110,29 @@ resolve_args_comma_functor(S, B, Scope, Res) :-
 
 resolve_args_comma_functor(S, B, Scope, Res) :-
 	functor(B, Nm, _),
-	Nm /= ',',
+	(Nm \= ','),
 	resolve_arg(Scope, B, Bd, Commands),
-	append(Commands, Bd, Res).
+	append([Bd], Commands, Res).
 
-generate_def(S, rule(P, B), Scope)) :- 
+generate_def(S, fact(P), Scope) :- 
+	P =.. [Nm|ArgLstIn], resolve_args(Scope, ArgLstIn, ArgLst, Commands),
+	make_predicate_name(Nm, Scope, NewName),
+	make_from_fact(NewName, ArgLst, Commands, Res),
+	out_stream(S, Out), write(Out, Res), write(Out, '.\n').
+
+generate_def(S, rule(P, B), Scope) :-
 	P =.. [Nm|ArgLstIn], resolve_args(Scope, ArgLstIn, ArgLst, Commands),
 	resolve_args_comma_functor(S, B, Scope, Res),
 	append(Commands, Res, Body),
 	make_predicate_name(Nm, Scope, NewName),
 	Pd =.. [NewName|ArgLst],
-	CBdy =.. [','|Body], %Again this might not work
-	out_stream(S, Out), write(Out, (Pd :- CBdy), write(Out, '\n').
+	list_to_comma_functor(Body, CBdy),
+	out_stream(S, Out), write(Out, (Pd :- CBdy)), write(Out, '.\n').
 
-generate_def(S, class_def(H,B), Scope) :- 
-	make_class(S, class_def(H,B), Scope), 
-	get_class_name(class_def(H,B), N), 
-	new_scope(B, Scope, N, NewScope), 
+generate_def(S, class_def(H,B), Scope) :-
+	make_class(S, class_def(H,B), Scope),
+	get_class_name(class_def(H,B), N),
+	new_scope(B, Scope, N, NewScope),
 	generate_defs(S, B, NewScope).
 
 %Fields don't generate anything
