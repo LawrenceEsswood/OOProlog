0e51ed2ac44dfdced0a5c14c46d2c2c1d0acf9a6
LawrenceEsswood <le277@cam.ac.uk>
2014-12-21 17:54:12 +0000

    Single inheritance of fields and predicates

--- src/generate.pl
+++ src/generate.pl
@@ -1,46 +1,81 @@
 :- ['./parse.pl'].
+:- ['./utils.pl'].
 
-%main :- quick('./tests/test2',':- [\'standard.out.pl\'].\n').
-main :- quick('./standard').
+main :- quick('./tests/test3',':- [\'standard.out.pl\'].\n:- interpret.\n').
+%main :- quick('./standard').
 
 quick(X, Extras) :- atom_concat(X, '.oopl', X1), atom_concat(X, '.out.pl', X2), compile(X1, X2, Extras).
 quick(X) :- quick(X,'').
 
 compile(In, Out, Extras) :- 
 	open(In, read , S), parse_defs((S, user_output, _), Defs, end_of_file), !,
-	open(Out, write , S2), write(S2, Extras), generate_defs_top((user_input, S2, _), Defs, top), !.
+	open(Out, write , S2), write(S2, Extras), generate_defs_top((user_input, S2, _), Defs), !.
 
 % Helper predicates (these should be moved into their own file)
-list_to_comma_functor([X],X).
-list_to_comma_functor([X,Y|Zs], (X,C)) :- list_to_comma_functor([Y|Zs], C).
 
-add_qoutes(X,Y) :- atomic_list_concat(['\'', X, '\''], Y).
-
-% Creates a tuple of the original name of a predicate, and its scoped name
-pair_predicate(Scope, Name, (ID, Resolved)) :-
-	make_predicate_name(Name, Scope, Resolved),
-	add_qoutes(Name, ID).
-
-get_predicate_pair(Scope, fact(P), Pair) :- functor(P, Name, _), pair_predicate(Scope, Name, Pair).
-get_predicate_pair(Scope, rule(P, _), Pair) :- functor(P, Name, _), pair_predicate(Scope, Name, Pair).
+% Creates a tuple of the original predicate, and its scoped name
+pair_predicate(Scope, Name, (Name, Resolved)) :-
+	make_predicate_name(Name, Scope, Resolved).
 
 %gets a field name and qoutes its
 get_field_name_qoute(X, Y) :- get_field_name(X, Z), add_qoutes(Z, Y).
 
+%TODO allow more than one parent
+find_parents_definitions(_, [], [],[],[]).
+find_parents_definitions(Scope, [Parent], Fields, Predicates, SuperPreds) :-
+	(lookup(Scope, class(Parent), PDefScope) ; throw_atoms(['Could not find a class of name ', Parent,'.'])),
+	(scope_of(PDefScope, Parent, ParentScope) ; throw_atoms([Parent, ' is not a class.'])),
+	scope_header(ParentScope, ParentHeader),
+	find_class_definitions(ParentScope, ParentHeader, Fields, Predicates, SuperPreds).
+
+make_super_call(CRes, PRes, N , L :- R) :- 
+	tmp_lst(N, Ts),
+	L =.. [CRes, 'This'|Ts],
+	R =.. [PRes, 'This'|Ts].
+
+predicate_inherit([], TotalPairs, TotalPairs, []).
+predicate_inherit([(pred(CName,NC), CRes)|Ps], ParentPs, TotalPairs, [Sup|SuperPreds]) :- 
+	member((pred(CName,NP), PRes), ParentPs),
+	!,
+	delete(ParentPs, (pred(CName,NP), PRes), ParentPsWO),
+	make_super_call(CRes, PRes, NP, Sup),
+	predicate_inherit([(pred(CName,NC), CRes)|Ps], ParentPsWO, TotalPairs, SuperPreds).
+predicate_inherit([(CName, CRes)|Ps], ParentPs, [(CName, CRes)|TotalPairs], SuperPreds) :-
+	predicate_inherit(Ps, ParentPs, TotalPairs, SuperPreds).
+
+
+find_class_definitions(Scope, Hchild, Fields, Predicates, SuperPreds) :-
+	scope_defines(Scope, Names),
+	filter(get_field_name_qoute,Names,ThisFields),
+	filter(pair_predicate(Scope), Names, ThisPredicates),
+	get_header_parents(Hchild, Parents),
+	find_parents_definitions(Scope, Parents, ParentFields, ParentPredicates, ParentSuperPreds),
+	(intersection(ThisFields, ParentFields,[]) ; throw('Cannot redefine a parents field.')),
+	append(ParentFields, ThisFields, Fields), %Might allow redefinition, use union
+	predicate_inherit(ThisPredicates, ParentPredicates, Predicates, ThisSuperPreds),
+	append(ParentSuperPreds, ThisSuperPreds, SuperPreds).
+
+not_same_pred_name(Nm, (pred(Nm2,N), Res), (pred(Nm2,N), Res)) :- Nm \= Nm2.
+
+get_unique_predicates([],[]).
+get_unique_predicates([(pred(Nm,_),Res)|Ps], [(Nm,Res)|OPs]) :-
+	filter(not_same_pred_name(Nm),Ps,FPs),
+	get_unique_predicates(FPs, OPs).
+
 % Generates the rule that constructs a class type
-make_class(S, Def, ScopeAbove, Scope) :- 
+make_class(S, Def, ScopeAbove, Scope, SuperPreds) :-
 	get_class_name(Def, Name),
-	get_class_body(Def, Defs),
-	get_all_names_of(get_field_name_qoute, Defs, Fields),
-	get_all_names_of(get_predicate_pair(Scope), Defs, Predicates),
+	get_class_header(Def, Hdr),
+	find_class_definitions(Scope, Hdr, Fields, Predicates, SuperPreds),
+	get_unique_predicates(Predicates, UPredicates),
 	make_class_type_functor(Name, ScopeAbove, F),
 	make_class_type_args(ScopeAbove, Args),
-	make_class_type_body(Name, Fields, Predicates, Bdy, none),
+	make_class_type_body(Name, Fields, UPredicates, Bdy, none),
 	P =.. [F | Args],
 	out_stream(S, Out), write(Out, (P :- Bdy)), write(Out, '.\n').
 
 % The name of a predicate after generation
-make_predicate_name(PredicateName, Scope, Result) :- scope_prefix(Scope, Prefix), atomic_list_concat(['p',Prefix, PredicateName], Result).
+make_predicate_name(pred(PredicateName, _), Scope, Result) :- scope_prefix(Scope, Prefix), atomic_list_concat(['p',Prefix, PredicateName], Result).
 % The name of the rule for getting a classes type info
 make_class_type_functor(ClassName, Scope, Result) :- scope_prefix(Scope, Prefix), atomic_list_concat([class_def, Prefix, ClassName], Result).
 % The compound the represents a class type 
@@ -48,7 +83,7 @@ make_class_type_args(Scope, Bdy) :- add_this(['T'], Scope, Bdy).
 % Easier to just use new
 make_class_type_body(Name, Fields, Predicates, Bdy, Constructor) :- 
 	expand_new(new('X', 'T', Name, Fields, Predicates, none, none, Constructor), _, NewComs),
-	list_to_comma_functor([class_def_classType('X') | NewComs], Bdy). 
+	list_to_comma_functor([class_def__classType('X') | NewComs], Bdy). 
 
 expand_new(Command, Con, [build_class(Type, Class), ::(Type, Constructor, Con) , ((Con = none) ; Call)]) :- 
 	add_qoutes('Constructor', Constructor),
@@ -65,42 +100,58 @@ get_class_body(class_def(_,X),X).
 
 % Gets a name from a particular definition
 get_class_name(class_def(class_head(X,_),_), X).
-get_fact_name(fact(P), Name) :- functor(P, Name, _).
-get_rule_name(rule(P,_), Name) :- functor(P, Name, _).
+get_class_header(class_def(Hd, _), Hd).
+get_header_parents(class_head(_,P), P).
+get_header_name(class_head(N,_), N).
+get_fact_name(fact(P), Name/N) :- functor(P, Name, N).
+get_rule_name(rule(P,_), Name/N) :- functor(P, Name, N).
 get_field_name(field(Name), Name).
-get_all_names_of(Rule, [D|Defs],[N|Names]) :- call(Rule, D, N), !, get_all_names_of(Rule, Defs, Names).
-get_all_names_of(Rule, [_|Defs], Names) :- get_all_names_of(Rule, Defs, Names).
-get_all_names_of(_, [], []).
 
 % Gets a name from a definition
 get_name(Def, class(Name)) :- get_class_name(Def, Name).
-get_name(Def, pred(Name)) :- get_fact_name(Def, Name).
-get_name(Def, pred(Name)) :- get_rule_name(Def, Name).
+get_name(Def, pred(Name,N)) :- get_fact_name(Def, Name/N).
+get_name(Def, pred(Name,N)) :- get_rule_name(Def, Name/N).
 get_name(Def, field(Name)) :- get_field_name(Def, Name).
 
 % Gets all the names defined at this scope
 get_defined_names([],[]).
 get_defined_names([D|Ds],[N|Ns]) :- get_name(D,N), get_defined_names(Ds, Ns).
 
-%Gets a scopes prefix
-scope_prefix(scp(Name,_,_), Name).
+% Getters for scope
+scope_prefix(scp(Name,_,_,_,_), Name).
 scope_prefix(top,'_').
-scope_defines(scp(_,Names,_), Names).
-scope_parent(scp(_,_,P), P).
-scope_is_top(scp(_,_,top)).
+scope_defines(scp(_,_,Names,_,_), Names).
+scope_parent(scp(_,_,_,_,P), P).
+scope_is_top(scp(_,_,_,_,top)).
+scope_header(scp(_,Hdr,_,_,_), Hdr).
+scope_class_scopes(scp(_,_,_,CS,_), CS).
+
+% Creates a new scope
+new_scope(Defs, Hd, OldScope, Name, S) :- 
+	S = scp(NewName, Hd, Defined, ClassScopes, OldScope),
+	scope_prefix(OldScope, OldName),
+	atomic_list_concat(['_', Name, OldName], NewName),
+	get_defined_names(Defs, Defined),
+	all_new_class_scopes(Defs, S, ClassScopes).
+
+all_new_class_scopes(Defs, Scope, CSS) :- filter(new_class_scope(Scope), Defs, CSS).
+new_class_scope(ScopeAbove, C, cs(N, ClassScope)) :-
+	get_class_name(C, N),
+	get_class_header(C, H),
+	get_class_body(C, B),
+	new_scope(B, H, ScopeAbove, N, ClassScope).
 
 % looks up a name to get its containing scope
 lookup(Scope, Arg, Scope) :- scope_defines(Scope, Names), member(Arg, Names), !.
 lookup(Scope, Arg, Res) :- scope_parent(Scope, Next), lookup(Next, Arg, Res).
 
-% Creates a new scope
-new_scope(Defs, OldScope, Name, scp(NewName,Defined, OldScope)) :- 
-	scope_prefix(OldScope, OldName),
-	atomic_list_concat(['_', Name, OldName], NewName), 
-	get_defined_names(Defs, Defined).
+%trys to find a scope of a certain class
+scope_of(Scope, Pname, Scp) :- scope_class_scopes(Scope, CS), member(cs(Pname, Scp), CS).
+
+%Walks the parse tree finding out names to create scope
 
 % Top level predicate for generating prolog
-generate_defs_top(S, Defs, Scope) :- new_scope(Defs, Scope, '', NewScope), generate_defs(S, Defs, NewScope).
+generate_defs_top(S, Defs) :- new_scope(Defs, top, top, '', NewScope), generate_defs(S, Defs, NewScope).
 
 % Generates prolog from a list of definitions
 generate_defs(S, [Def|Defs], Scope) :- generate_def(S, Def, Scope), generate_defs(S, Defs, Scope).
@@ -123,7 +174,7 @@ resolve_arg(Scope, Reps, Reps3, X::Y, Tmp, Cmds3) :-
 	resolve_arg(Scope, Reps2, Reps3, Y, Yd, Cmds2),
 	append(Cmds2, [::(Xd,Yd,Tmp)|Cmds1], Cmds3).
 
-%Atoms might refer to a class FIXME: also predicates when used with first order
+%Atoms might refer to a class FIXME: also predicates when used with first order predicates
 resolve_arg(Scope, Reps, [(atom(X),Tmp)|Reps], atom(X), Tmp, [C]) :-
 	lookup(Scope, class(X), _),!,
 	make_class_type_functor(X, Scope, F),
@@ -145,8 +196,9 @@ resolve_arg(_, R, R, tmp(Arg), Arg, []) :- !.
 % This is the case where we have a compound, might be need to add a prefix and/or 'This'
 resolve_arg(Scope, Reps, RepsOut, P, Pd, Commands) :- 
 	P =.. [Nm|Args],
-	lookup(Scope, pred(Nm), PredScope), !,
-	make_predicate_name(Nm, Scope, NewName),
+	length(Args, N),
+	lookup(Scope, pred(Nm, N), PredScope), !,
+	make_predicate_name(pred(Nm, N), Scope, NewName),
 	resolve_args(Scope, Reps, RepsOut, Args, Args2, Commands),
 	add_this(Args2, PredScope, Args3),
 	Pd =.. [NewName|Args3].
@@ -188,30 +240,35 @@ add_this(Ls, Scope, Ls) :- scope_is_top(Scope).
 add_this(Ls, Scope, ['This'|Ls]) :- not(scope_is_top(Scope)).
 
 generate_def(S, fact(P), Scope) :- 
-	P =.. [Nm|ArgLstIn], 
+	P =.. [Nm|ArgLstIn],
+	length(ArgLstIn, N),
 	resolve_args(Scope, [], _, ArgLstIn, ArgLst, Commands),
-	make_predicate_name(Nm, Scope, NewName),
+	make_predicate_name(pred(Nm, N), Scope, NewName),
 	add_this(ArgLst, Scope, ArgLst2),
 	make_from_fact(NewName, ArgLst2, Commands, Res),
 	out_stream(S, Out), write(Out, Res), write(Out, '.\n').
 
 %FIXME, we can look through all the replacements to work out if we need this
 generate_def(S, rule(P, B), Scope) :-
-	P =.. [Nm|ArgLstIn], 
+	P =.. [Nm|ArgLstIn],
+	length(ArgLstIn, N),
 	resolve_args(Scope, [], OutReps, ArgLstIn, ArgLst, Commands),
 	resolve_args_comma_functor(B, Scope, OutReps, _, Res),
-	append(Commands, Res, Body),
-	make_predicate_name(Nm, Scope, NewName),
+	append(Res, Commands, Body),
+	make_predicate_name(pred(Nm, N), Scope, NewName),
 	add_this(ArgLst, Scope, ArgLst2),
 	Pd =.. [NewName | ArgLst2],
 	list_to_comma_functor(Body, CBdy),
 	out_stream(S, Out), write(Out, (Pd :- CBdy)), write(Out, '.\n').
 
-generate_def(S, class_def(H,B), Scope) :-
-	get_class_name(class_def(H,B), N),
-	new_scope(B, Scope, N, NewScope),
-	make_class(S, class_def(H,B), Scope, NewScope),
-	generate_defs(S, B, NewScope).
+generate_def(S, C, Scope) :-
+	get_class_name(C, N),
+	scope_of(Scope, N, NewScope),
+	make_class(S, C, Scope, NewScope, Supers),
+	get_class_body(C, B),
+	generate_defs(S, B, NewScope),
+	out_stream(S, Out),
+	write_list(Out, Supers).
 
 %Fields don't generate anything
 generate_def(_, field(_), Scope) :- not(scope_is_top(Scope)); throw('Fields must occur within a class').
---
+++ src/utils.pl
@@ -0,0 +1,22 @@
+% Turns a list into a conjunction of n terms
+list_to_comma_functor([X],X).
+list_to_comma_functor([X,Y|Zs], (X,C)) :- list_to_comma_functor([Y|Zs], C).
+
+% Throws an exception with the concatanation of n terms
+throw_atoms(Atms) :- atomic_list_concat(Atms, Msg), throw(Msg).
+
+% Creates a list of n unique temps
+tmp_lst(0, []).
+tmp_lst(N, [T|Ts]) :- N >= 0, Nd is N-1, atom_concat('V',N,T), tmp_lst(Nd, Ts).
+
+% Writes out a list of terms seperated by .\n
+write_list(_, []).
+write_list(Out, [L|LS]) :- write(Out, L), write(Out, '.\n'), write_list(Out, LS).
+
+% A map function that simply removes elements that do not satisfy the rule
+filter(Rule, [D|Defs],[N|Names]) :- call(Rule, D, N), !, filter(Rule, Defs, Names).
+filter(Rule, [_|Defs], Names) :- filter(Rule, Defs, Names).
+filter(_, [], []).
+
+% Add qoutes to an atom
+add_qoutes(X,Y) :- atomic_list_concat(['\'', X, '\''], Y).
